5ba23901f19a1a480b53156d89a08df5de3f7f7e
crdtrd <crdtrd@gmail.com>
2026-05-20 18:13:40 -0700

    added new prot attribute values and made change to enchantment selection system

--- src/main/java/com/drtdrc/crdtrdsmod/enchantingencore/mixin/CombatRulesMixin.java
+++ src/main/java/com/drtdrc/crdtrdsmod/enchantingencore/mixin/CombatRulesMixin.java
@@ -1,5 +1,7 @@
 package com.drtdrc.crdtrdsmod.enchantingencore.mixin;
 
+import com.drtdrc.crdtrdsmod.core.CrdtrdsMod;
+import com.drtdrc.crdtrdsmod.core.ModConfig;
 import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.CombatRules;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +14,15 @@ public abstract class CombatRulesMixin {
 
     @Inject(method = "getDamageAfterMagicAbsorb", at = @At("HEAD"), cancellable = true)
     private static void crdtrdsmod$modifyMagicAbsorb(float damage, float protection, CallbackInfoReturnable<Float> cir) {
-        float f = Mth.clamp(protection, 0.0F, 35.0F);
-        cir.setReturnValue(damage * (1.0F - f / 35.35F));
+        if ("casual".equals(ModConfig.active().enchantingEncore)) {
+            float f = Mth.clamp(protection, 0.0F, 35.0F);
+            cir.setReturnValue(damage * (1.0F - f / 35.35F));
+        }
+        else {
+            float f = Mth.clamp(protection, 0.0F, 99.0F);
+            cir.setReturnValue(damage * (1.0F - f / 100.0F));
+            CrdtrdsMod.LOGGER.info(String.valueOf( (1.0F - f / 100.0F)));
+            CrdtrdsMod.LOGGER.info(String.valueOf( damage * (1.0F - f / 100.0F)));
+        }
     }
 }
--- src/main/java/com/drtdrc/crdtrdsmod/enchantingencore/mixin/EnchantmentHelperMixin.java
+++ src/main/java/com/drtdrc/crdtrdsmod/enchantingencore/mixin/EnchantmentHelperMixin.java
@@ -1,22 +1,42 @@
 package com.drtdrc.crdtrdsmod.enchantingencore.mixin;
 
+import com.drtdrc.crdtrdsmod.core.ModConfig;
 import com.drtdrc.crdtrdsmod.enchantingencore.EnchantmentSelectionBiasContext;
+import com.google.common.collect.Lists;
+import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
+import net.minecraft.util.random.WeightedRandom;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantable;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.ToIntFunction;
+import java.util.stream.Stream;
+
+import static net.minecraft.world.item.enchantment.EnchantmentHelper.filterCompatibleEnchantments;
+import static net.minecraft.world.item.enchantment.EnchantmentHelper.getAvailableEnchantmentResults;
 
 @Mixin(EnchantmentHelper.class)
 public abstract class EnchantmentHelperMixin {
+
+    @Shadow
+    public static boolean isEnchantmentCompatible(Collection<Holder<Enchantment>> enchants, Holder<Enchantment> target) {
+        throw new UnsupportedOperationException("Implemented via mixin");
+    }
 
     @Inject(
             method = "getEnchantmentCost",
@@ -27,7 +47,8 @@ public abstract class EnchantmentHelperMixin {
         Enchantable enchantable = stack.get(DataComponents.ENCHANTABLE);
         if (enchantable == null) {
             cir.setReturnValue(0);
-        } else {
+        }
+        else if ("casual".equals(ModConfig.active().enchantingEncore)) {
             if (bookshelfCount > 50) {
                 bookshelfCount = 50;
             }
@@ -38,17 +59,80 @@ public abstract class EnchantmentHelperMixin {
                 cir.setReturnValue(slotIndex == 1 ? i * 2 / 3 + 1 : bookshelfCount);
             }
         }
+        else {
+            if (bookshelfCount > 500) {
+                bookshelfCount = 500;
+            }
+            int i = random.nextInt(4) + 1 + (bookshelfCount >> 1);
+            if (slotIndex == 0) {
+                cir.setReturnValue(Math.max(i / 3, 1));
+            } else {
+                cir.setReturnValue(slotIndex == 1 ? i * 2 / 3 + 100 : bookshelfCount * 2);
+            }
+        }
     }
 
-    @ModifyArg(
+//    @ModifyArg(
+//            method = "selectEnchantment",
+//            at = @At(
+//                    value = "INVOKE",
+//                    target = "Lnet/minecraft/util/random/WeightedRandom;getRandomItem(Lnet/minecraft/util/RandomSource;Ljava/util/List;Ljava/util/function/ToIntFunction;)Ljava/util/Optional;"
+//            ),
+//            index = 2
+//    )
+//    private static ToIntFunction<EnchantmentInstance> crdtrdsmod$biasWeightFunction(
+//            ToIntFunction<EnchantmentInstance> original
+//    ) {
+//        return entry -> {
+//            int base = original.applyAsInt(entry);
+//            int bonus = EnchantmentSelectionBiasContext.bonus(entry.enchantment());
+//            return Math.max(1, base + bonus);
+//        };
+//    }
+
+    @Inject(
             method = "selectEnchantment",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/util/random/WeightedRandom;getRandomItem(Lnet/minecraft/util/RandomSource;Ljava/util/List;Ljava/util/function/ToIntFunction;)Ljava/util/Optional;"
-            ),
-            index = 2
+            at = @At("HEAD"),
+            cancellable = true
     )
-    private static ToIntFunction<EnchantmentInstance> crdtrdsmod$biasWeightFunction(
+    private static void crdtrdsmod$newSelectEnchantment(RandomSource random, ItemStack itemStack, int enchantmentCost, Stream<Holder<Enchantment>> source, CallbackInfoReturnable<List<EnchantmentInstance>> cir) {
+        List<EnchantmentInstance> results = Lists.<EnchantmentInstance>newArrayList();
+        Enchantable enchantable = itemStack.get(DataComponents.ENCHANTABLE);
+        if (enchantable == null) {
+            cir.setReturnValue(results);
+        } else {
+            enchantmentCost += 1 + random.nextInt(enchantable.value() / 4 + 1) + random.nextInt(enchantable.value() / 4 + 1);
+            float randomSpan = (random.nextFloat() + random.nextFloat() - 1.0F) * 0.15F;
+            enchantmentCost = Mth.clamp(Math.round(enchantmentCost + enchantmentCost * randomSpan), 1, Integer.MAX_VALUE);
+            List<EnchantmentInstance> enchantments = getAvailableEnchantmentResults(enchantmentCost, itemStack, source);
+            if (!enchantments.isEmpty()) {
+                WeightedRandom.getRandomItem(random, enchantments, crdtrdsmod$biasedWeightFunction(EnchantmentInstance::weight)).ifPresent(results::add);
+
+                while (random.nextInt(50) <= enchantmentCost) {
+
+                    // get additional enchantment
+                    EnchantmentInstance additionalEnchantment = WeightedRandom.getRandomItem(random, enchantments, crdtrdsmod$biasedWeightFunction(EnchantmentInstance::weight)).orElseThrow();
+                    //check if additional enchantment is compatible and add to result if so
+                    boolean compatible = true;
+                    for (EnchantmentInstance e : results) {
+                        if (!Enchantment.areCompatible(e.enchantment(),additionalEnchantment.enchantment())) {
+                            compatible = false;
+                            break;
+                        }
+                    }
+                    if (compatible) results.add(additionalEnchantment);
+
+                    // vanilla: divide enchantment cost by 2 to limit the number of enchantments
+                    enchantmentCost /= 2;
+                }
+            }
+
+            cir.setReturnValue(results);
+        }
+    }
+
+    @Unique
+    private static ToIntFunction<EnchantmentInstance> crdtrdsmod$biasedWeightFunction(
             ToIntFunction<EnchantmentInstance> original
     ) {
         return entry -> {
